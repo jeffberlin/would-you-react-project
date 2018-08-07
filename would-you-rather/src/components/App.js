e2dba0b3ee844c8cd543aa040bcebd5922650238
Jeff Berlin <jeff@jbrally.com>
2018-08-07 17:52:06 -0400

    Setting up structure

--- would-you-rather/src/components/App.js
+++ would-you-rather/src/components/App.js
@@ -6,16 +6,15 @@ import { connect } from 'react-redux'
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+        <Fragment>
+          <div>
+
+          </div>
+        </Fragment>
+      </Router>
     );
   }
 }
 
-export default App;
+export default connect()(App)
