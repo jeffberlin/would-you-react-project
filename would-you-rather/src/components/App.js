7d53aac98fbd508fa542ef88f064420864e43b13
Jeff Berlin <jeff@jbrally.com>
2018-08-07 17:40:08 -0400

    Installing dependencies

--- would-you-rather/src/components/App.js
+++ would-you-rather/src/components/App.js
@@ -1,5 +1,7 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component, Fragment } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { connect } from 'react-redux'
+
 
 class App extends Component {
   render() {
